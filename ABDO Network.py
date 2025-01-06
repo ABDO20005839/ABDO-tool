246d2e8cb3b72b3026da75a88024afe1b556a696
ABDO20005839 <150443696+ABDO20005839@users.noreply.github.com>
2025-01-06 10:45:08 +0200

    Add files via upload
    It is a tool to hack passwords of the networks you are connected to now. This means that if I am connected to a network called Network, and I run the tool and type the name of the network that I am now connected to, it will show me the password for the network. It can be tried in cafes and places where you always have to pay money to enjoy the Internet, where you can pay once to take the network, then run the tool and write the name of the network, then you will have the password with you easily.

---
+++ ABDO Network.py
@@ -0,0 +1,39 @@
+import subprocess
+from colorama import Fore, Style, init
+import pyfiglet
+def print_header():
+    # طباعة اسم الأداة بشكل كبير
+    ascii_art = pyfiglet.figlet_format("ABDO Network Tool", font="slant")
+    print(Fore.RED + Style.BRIGHT + ascii_art)
+    print(Fore.RED + "=" * 50)
+    print(Fore.GREEN + "💀👻👺👽 Welcome to the ABDO Tool! 👻💀👽\n")
+    
+def get_wifi_password(ssid):
+    # تنفيذ الأمر للحصول على معلومات الشبكة
+    command = f"netsh wlan show profiles {ssid} key=clear"
+    
+    try:
+        # تنفيذ الأمر باستخدام subprocess
+        output = subprocess.check_output(command, shell=True, text=True)
+        
+        # البحث عن كلمة المرور في المخرجات
+        for line in output.splitlines():
+            if "Key Content" in line:
+                password = line.split(":")[1].strip()  # استخراج كلمة المرور
+                return password
+    except subprocess.CalledProcessError:
+        print("Unable to find the specified SSID or other error.")
+    return None
+
+def main():
+    print_header()
+    ssid = input("Enter the Wi-Fi network name (SSID): ")
+    password = get_wifi_password(ssid)
+    
+    if password:
+        print(f"The password for '{ssid}' is: {password}")
+    else:
+        print(f"No password found for '{ssid}' or the SSID does not exist.")
+
+if __name__ == "__main__":
+    main()
